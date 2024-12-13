b339c0bce734f069e11247c3f38faf170bbbd6e8
alex2000 <jenjer444@gmail.com>
2024-12-14 01:39:46 +0200

    task3 initial

---
+++ task3/vite-proj/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div class="container">
+
+        <div class="search-section d-flex">
+          <form class="search-form d-flex">
+            <input type="text" placeholder="Country"/>
+            <input type="submit" value="Send"/>
+          </form>
+          <button>Reset</button>
+        </div>
+
+        <div class="table-section">
+    
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
